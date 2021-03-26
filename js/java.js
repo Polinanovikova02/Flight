75d81d0f08ec1684dc39bcf24fbabe5b1b57ea6d
Polinanovikova02 <novikp02@mail.ru>
2021-03-26 07:09:29 +0300

    +

--- js/java.js
+++ js/java.js
@@ -7,10 +7,11 @@ $(document).ready(function(){
     $(".white").addClass("exit_white");
 
     setTimeout(function(){
-      $('body').css('background', 'linear-gradient(180deg, #000000 0%, #128BB6 50%)');
+      $('body').css('background', 'linear-gradient(180deg, #000000 19.79%, #128BB6 100%)');
     }, 1800);
-    // $(".triangle").addClass("entrance_surface");
-    $(".triangle2").addClass("entrance_surface");
+    $(".circle1, .circle2, .circle3").addClass("entrance_surface");
+    $(".triangle, .triangle2, .triangle3, .triangle4").addClass("entrance_surface");
+    $(".line_centre, .centre").addClass("entrance_surface");
     $(".cross1, .cross2").addClass("entrance_surface");
     $(".b-top, .b-right, .b-bottom, .b-left").addClass("entrance_surface");
     $(".interface").addClass("entrance_surface");
